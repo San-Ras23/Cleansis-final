45bfe8fae56be7937c3ecc48c592570b60eca9a4
Sandra Rasgido <145142989+San-Ras23@users.noreply.github.com>
2023-12-10 01:51:27 +0000

    cambios de clase

--- app_productos/models.py
+++ app_productos/models.py
@@ -10,22 +10,22 @@ class Producto(Model):
     """
     nombre  =  models.CharField(max_length=100, default="Nombre x")
     categoria  =  models.CharField(max_length=100, default="Categoria x", blank=False, null=False)
-    codigo  =  models.IntegerField (blank=False,null=False)
-    precio  =  models.IntegerField (blank=False,null=False)
+    codigo  =  models.FloatField (blank=False,null=False)
+    precio  =  models.FloatField (blank=False,null=False, default=100.00)
 
 
     # podemos crear la tabla con un nombre especifico pero se lo tenemos
     # que indicar directamente en la metaclase
 
 class Meta:
-    db_table = "Product_table"
+    db_table = "product_table"
 
 
     def __str__(self):
-        return f"El producto es: {self.nombre}, categoria:{self.categoria}"
+            return f"El producto es: {self.nombre}, categoria:{self.categoria}"
 
     def get_fields(self):
-        return [
-            (field.verbose_name, field.value_from_object(self))
-            for field in self.__class__._meta.fields[1:]
-        ]
+            return [
+                (field.verbose_name, field.value_from_object(self))
+                for field in self.__class__._meta.fields[1:]
+            ]
