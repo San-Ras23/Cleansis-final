2e98dd9a477671906b0fd7841fdbd0f848eb1389
CeciliaMCassain <ceciliacassain@gmail.com>
2023-12-13 12:05:11 -0300

    update database settings

--- Cleansis_project/settings.py
+++ Cleansis_project/settings.py
@@ -86,12 +86,15 @@ WSGI_APPLICATION = "Cleansis_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'MCeciliaMCassain$Cleansis_project',
+            'USER': 'MCeciliaMCassain',
+            'PASSWORD': 'pirulo1234',
+            'HOST': 'MCeciliaMCassain.mysql.pythonanywhere-services.com',
+            'PORT': '3306',
+        }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
